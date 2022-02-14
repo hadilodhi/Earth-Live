d37b5cee13d12daf1261b008932d7b2d6d3fef26
Hadi <hadirocks2@gmail.com>
2022-02-14 13:32:32 +0800

    Adding the basics

---
+++ Assets/TerrainFace.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TerrainFace
+{
+    Mesh mesh;
+    int resolution;
+    Vector3 localUp;
+    Vector3 axisA;
+    Vector3 axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    public static Vector3 pointOnUnitSphere(Vector3 p)
+    {
+        float x2 = p.x * p.x;
+        float y2 = p.y * p.y;
+        float z2 = p.z * p.z;
+        float x = p.x * (float)Math.Sqrt(1 - (y2 + z2) / 2 + (y2 * z2) / 3);
+        float y = p.y * (float)Math.Sqrt(1 - (z2 + x2) / 2 + (z2 * x2) / 3);
+        float z = p.z * (float)Math.Sqrt(1 - (x2 + y2) / 2 + (x2 * y2) / 3);
+        return new Vector3(x,y,z);
+    }
+
+    public static Vector2 PointToCoordinate(Vector3 pointOnUnitSphere)
+    {
+        float latitude = (float)Math.Asin(pointOnUnitSphere.y);
+        float longitude = (float)Math.Atan2(pointOnUnitSphere.x, -pointOnUnitSphere.z);
+        return new Vector2(latitude, longitude);
+    }
+
+    public static Vector3 CoordinateToPoint(Vector2 coordinate)
+    {
+        float y = (float)Math.Sin(coordinate.x);
+        float r = (float)Math.Cos(coordinate.x);
+        float x = (float)Math.Sin(coordinate.y) * r;
+        float z = -(float)Math.Cos(coordinate.y) * r;
+        return new Vector3(x, y, z);
+    }
+
+    public void ConstructMesh()
+    {
+        Vector3[] vertices = new Vector3[resolution*resolution];
+        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+        int triIndex = 0;
+
+        for (int y = 0; y  < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x+y*resolution;
+                Vector2 percent = new Vector2(x , y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x-.5f) * 2 * axisA + (percent.y-.5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                vertices[i] = pointOnUnitSphere;
+                // vertices[i] = pointOnUnitSphere(pointOnUnitCube);
+
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    triangles[triIndex] = i;
+                    triangles[triIndex+1] = i + resolution + 1;
+                    triangles[triIndex+2] = i + resolution;
+
+                    triangles[triIndex+3] = i;
+                    triangles[triIndex+4] = i + 1;
+                    triangles[triIndex+5] = i + resolution + 1;
+                    triIndex += 6;
+                }
+            }
+        }
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
